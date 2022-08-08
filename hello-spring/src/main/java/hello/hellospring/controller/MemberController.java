9c3b1f5a57505f9391e409c0f91660970e9ff1ee
woojin2017 <76552988+woojin2017@users.noreply.github.com>
2022-08-08 17:46:31 +0900

    Add files via upload

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,9 +1,15 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+/*
+    post : 데이터를 form에 넣어서 전달할 때 사용
+    get : 조회할 때 주로 사용
+ */
 @Controller
 public class MemberController {
     private final MemberService memberService;
@@ -13,4 +19,18 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String creeteForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return  "redirect:/";
+    }
 }
